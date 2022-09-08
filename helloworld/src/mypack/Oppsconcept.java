b415e3c0d90859ef543f2a6d2219c3d12b44465d
Amrutha gowda <Amrutha gowda@MSI>
2022-09-08 21:25:43 +0530

    first

---
+++ helloworld/src/mypack/Oppsconcept.java
@@ -0,0 +1,17 @@
+package mypack;
+
+public class Oppsconcept {
+	int year_ofpurchase;// datamembers or properties
+	String make;
+	int model;double cost;
+	public void display1() // member function
+	{
+		System.out.println("yearof purs"+2001);
+		System.out.println("make"+ " suzuki")
+	}
+void dispsly2()
+{
+	System.out.println("model"+1999);
+	System.out.println("make")
+}
+}
