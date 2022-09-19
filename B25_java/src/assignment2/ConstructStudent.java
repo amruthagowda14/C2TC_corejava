dd1d8c3651b8b8e4199d5b82a7118d66a2570667
Amrutha gowda <Amrutha gowda@MSI>
2022-09-19 11:26:17 +0530

    first commit

---
+++ B25_java/src/assignment2/ConstructStudent.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+public class ConstructStudent {
+
+	ConstructStudent(){
+		System.out.println("Student object is created");
+	}
+
+	public static void main(String[] args) {
+
+		ConstructStudent xc=new ConstructStudent();
+	}
+
+}
---
+++ B25_java/src/assignment2/ThisKeyWord.java
@@ -0,0 +1,19 @@
+package assignment2;
+
+public class ThisKeyWord {
+
+	int rollno;
+	public  ThisKeyWord(int rollno){
+		this.rollno=rollno;
+	}
+
+
+
+
+	public static void main(String[] args) {
+
+		ThisKeyWord rollno=new ThisKeyWord(5);
+		System.out.println("the rollno is ="+rollno.rollno);
+	}
+
+}
